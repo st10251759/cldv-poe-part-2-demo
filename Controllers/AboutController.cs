272bdddbe30b82ea35a5385a756e1c4ac2f49f59
st10251759 <ST10251759@vcconnect.edu.za>
2024-05-16 08:08:49 +0200

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Khumalo_Craft_P2.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
