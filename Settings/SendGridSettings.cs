47c376af29636ef5160388a6b9b49789fb9171c8
st10251759 <128587680+st10251759@users.noreply.github.com>
2024-06-23 20:48:08 +0200

    Create SendGridSettings.cs

---
+++ Settings/SendGridSettings.cs
@@ -0,0 +1,13 @@
+namespace Khumalo_Craft_P2.Settings
+{
+    public class SendGridSettings
+    {
+        public string FromEmail { get; set; }
+
+        public string EmailName { get; set; }
+
+        public string ApiKey { get; set; }
+
+
+    }
+}
